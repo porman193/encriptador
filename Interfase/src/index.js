421ad39a3b66675b2d4fbfb1f17a224d88a75a32
Juan Pablo Bautista <jbautistaclavijo@gmail.com>
2023-05-15 13:40:41 -0500

    Ya esta en funcionamiento las variable de copiar

--- Interfase/src/index.js
+++ Interfase/src/index.js
@@ -3,6 +3,8 @@ const btnEncriptar = document.getElementById('btn-encriptar');
 const btnDesencriptar = document.getElementById('btn-desencriptar');
 const resultado = document.getElementById('resultado');
 const btnCopiar = document.getElementById('btn-copiar');
+const divResultado = document.getElementById('divResultado');
+const containerResult = document.getElementById('containerResult');
 
 
 // guardar el texto dado por el usuario en una variable global
@@ -11,7 +13,8 @@ let textoAEncriptar = textoParaEncriptar.value;
 //funcionalidad del boton encriptar donde se llama a la funcion encriptar y muestra el resultado en el output de la pagina html
 btnEncriptar.addEventListener('click', () => {
     let texto = textoParaEncriptar.value;
-    resultado.textContent = encriptar(texto);
+    resultado.value = encriptar(texto);
+    containerResult.style.display = 'flex';
 });
 
 //funcionalidad del boton desencriptar donde se llama a la funcion desencriptar y muestra el resultado en el output de la pagina html
@@ -33,6 +36,8 @@ function encriptar(textoAEncriptar) {
     for (let i = 0; i < textoAEncriptar.length; i++) {
         textoEncriptado += String.fromCharCode(textoAEncriptar.charCodeAt(i)+1); // suma 1 al codigo ascii del caracter y lo convierte a caracter
     }
+    resultado.style.display = 'block';
+    divResultado.style.display = 'none';
     console.log(textoEncriptado);
     return textoEncriptado;
 }
@@ -54,5 +59,5 @@ function copiarAlPortapapeles(texto) {
 
 // Funcion que muestra el textto desencriptado en el output de la pagina html
  function mostrarDesencriptado(textoADesencriptar) {
-    resultado.textContent = desencriptar(textoADesencriptar);
+    resultado.value = desencriptar(textoADesencriptar);
 }
